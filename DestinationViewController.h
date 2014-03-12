9b26bf64f3686167fc811a3c88992d52b7cbcf89
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-12 09:04:28 -0500

    first commit

---
+++ DestinationViewController.h
@@ -0,0 +1,13 @@
+//
+//  DestinationViewController.h
+//  Adventure
+//
+//  Created by Manas Pradhan on 3/11/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DestinationViewController : UIViewController
+
+@end
